9c18225dea163e0fb49138bf2d5eb1125179a65c
Dalto Curvelano <daltojr@gmail.com>
2026-02-06 10:54:07 +0400

    Update formula to v0.2.4

--- Formula/pg_glimpse.rb
+++ Formula/pg_glimpse.rb
@@ -7,24 +7,24 @@ class PgGlimpse < Formula
   on_macos do
     on_arm do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "4f13bab5a4babae487eff312524266d7bcde5c4b8f0994d6668e8369417ea090"
+      sha256 "f0dbc14e4aeeb0f499c22f180b2c712ed362be82f50fd17789028e8de7da372e"
     end
 
     on_intel do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0da9c5727942503dcdcb661bacca4135c38e01b49e446c7e8aa88409516de3ae"
+      sha256 "69374152749512ba0ae20d169208f985a7bbda15f39b7bf5b7789eba020a653b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "22b36b9908f75783f01a134d8a3fc6275ffd541921323caed25ce235f2c0d713"
     end
 
     on_intel do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "5d0d791d5a935ef4c9e0e58c0508c34fced24b2c7472e933847d401ef479ef86"
     end
   end
 
