42552441b5593789ddd7f28399fbde9056718bc5
Dalto Curvelano <daltojr@gmail.com>
2026-02-12 12:19:54 +0400

    Update Homebrew formula for v0.5.1

--- Formula/pg_glimpse.rb
+++ Formula/pg_glimpse.rb
@@ -2,29 +2,29 @@ class PgGlimpse < Formula
   desc "Terminal-based PostgreSQL monitoring tool with live TUI"
   homepage "https://github.com/dlt/pg_glimpse"
   license "MIT"
-  version "0.5.0"
+  version "0.5.1"
 
   on_macos do
     on_arm do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "24ffbe371606e48c438d4fc406a20fd8c6324f5ed5b7a3febbe3c63200e70c19"
+      sha256 "ecece8d64cc94685148fe25e8b1fcf5360e083a52e732cc62c2ba85e0847afed"
     end
 
     on_intel do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "441b6ffbcb399190befddf07b7ac0bb8e9ef6847b2dcd6c638f6424c8e4ab4a7"
+      sha256 "3f3f7f2a276f57c48cefc7963ca03978e6d7427e2124d29e31cf8142f0e08cbe"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "40e1afe70f5b3c5aff1cdd9508f33ccb006662bbd2c00a66bc36f9ae9da33755"
+      sha256 "32b909551688676583d333b1d40aea612c559e00c6d0ebf773bcdbc39feadfdc"
     end
 
     on_intel do
       url "https://github.com/dlt/pg_glimpse/releases/download/v#{version}/pg_glimpse-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "21af727e2342e40f2a818c4027c80181ddc4d9be6207f783e2e49a856b6bb740"
+      sha256 "3807d54e5aa99ecb8f4587a30a0d1ecc9091a2cc65908e7860dfe8066eaa3abb"
     end
   end
 
